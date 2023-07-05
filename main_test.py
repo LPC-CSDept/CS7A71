fc2390be3e33fc86deb5c15e84f2156e53c11a38
Kyuwoong Lee <kwooler@gmail.com>
2023-07-05 15:38:32 -0700

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -6,15 +6,15 @@ import math
 
 
 def test_main_1():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    # datastr = '10 5 20 0 40 45 50 55 9 10\n'
-    # sys.stdin = io.StringIO(datastr)
+    # captureOut = io.StringIO()
+    # sys.stdout = captureOut
+    # # datastr = '10 5 20 0 40 45 50 55 9 10\n'
+    # # sys.stdin = io.StringIO(datastr)
 
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
+    # sys.stdout = sys.__stdout__
+    # print('Captured ', captureOut.getvalue())
+    # lines = captureOut.getvalue().split('\n')
+    # print(lines)
 
     numbers = [10, 5, 20, 0, 40, 45, 50, 55, 9, 10]
     ret = main.getFarNumber(numbers)
@@ -31,15 +31,15 @@ def test_main_1():
 
 
 def test_main_2():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    # datastr = '1 2 3 4 5 6 7 8 9 10\n'
-    # sys.stdin = io.StringIO(datastr)
+    # captureOut = io.StringIO()
+    # sys.stdout = captureOut
+    # # datastr = '1 2 3 4 5 6 7 8 9 10\n'
+    # # sys.stdin = io.StringIO(datastr)
 
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
+    # sys.stdout = sys.__stdout__
+    # print('Captured ', captureOut.getvalue())
+    # lines = captureOut.getvalue().split('\n')
+    # print(lines)
 
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20]
     ret = main.getFarNumber(numbers)
