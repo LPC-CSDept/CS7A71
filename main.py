53a20cb4e709a84b8a04704a8ac70621823a70bf
Kyuwoong Lee <kwooler@gmail.com>
2023-07-05 15:42:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ def main():
     # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20]
     ret = getFarNumber(numbers)
     print(f'Your return value is {ret}')
-    ##################################################
-    # Code your program here
-    ##################################################
-
 
 if __name__ == '__main__':
     main()
